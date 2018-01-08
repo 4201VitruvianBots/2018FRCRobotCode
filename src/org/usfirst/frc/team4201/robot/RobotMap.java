eaa05f75a55d49e7e8e2ee5cf019d67242db4482
Jonathan Dao <jonathandao0@gmail.com>
2018-01-08 13:12:03 -0800

    Jonathan Dao
    Initial Commit

---
+++ src/org/usfirst/frc/team4201/robot/RobotMap.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team4201.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	/*	ADDRESS VALUES
+	 *  
+	 *  CAN ADDRESSES
+	 *  0-19: Major Modules
+	 *  +0: PDP
+	 *  +1-9: VRMs
+	 *  +11-19: PCMs
+	 *  
+	 *  20-59: Motor Controllers
+	 *  +20-24(26): DriveTrain Motors
+	 */
+	
+	// Electrical Modules
+	public static final int PDP = 0;
+	public static final int VRMOne = 1;
+	
+	// Pneumatic Modules & their devices
+	public static final int PCMOne = 11;
+		public static final int driveTrainShifterForward = 0;
+		public static final int driveTrainShifterReverse = 1;
+	
+	// Motor Controllers
+	public static final int driveTrainLeftFront = 20;
+	public static final int driveTrainLeftRear = 21;
+	public static final int driveTrainRightFront = 22;
+	public static final int driveTrainRightRear = 23;
+	
+	// USB ADDRESSES
+	public static final int leftJoystick = 0;
+	public static final int rightJoystick = 1;
+	public static final int xBoxController = 2;
+}
---
+++ src/org/usfirst/frc/team4201/robot/subsystems/DriveTrain.java
@@ -0,0 +1,88 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team4201.robot.subsystems;
+
+import org.usfirst.frc.team4201.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class DriveTrain extends Subsystem {
+	
+	BaseMotorController[] driveMotors = {
+		new TalonSRX(RobotMap.driveTrainLeftFront),
+		new TalonSRX(RobotMap.driveTrainLeftRear),	// VictorSPX(RobotMap.driveTrainLeftRear),
+		new TalonSRX(RobotMap.driveTrainRightFront),
+		new TalonSRX(RobotMap.driveTrainRightRear)	// VictorSPX(RobotMap.driveTrainRightRear)
+	};
+	
+	//RobotDrive robotDrive = new RobotDrive(driveMotors[0], driveMotors[1], driveMotors[2], driveMotors[3]);
+	
+	DoubleSolenoid driveTrainShifters = new DoubleSolenoid(RobotMap.PCMOne, RobotMap.driveTrainShifterForward, RobotMap.driveTrainShifterReverse);
+	
+	public DriveTrain(){
+		super("Drive Train");
+		
+		// Set Motor Controller Control Mode
+		driveMotors[1].set(ControlMode.Follower, driveMotors[0].getDeviceID());
+		driveMotors[3].set(ControlMode.Follower, driveMotors[2].getDeviceID());
+
+		// Set Motor Controller Feedback Device
+		driveMotors[0].configSelectedFeedbackSensor(FeedbackDevice.Analog, 0, 0);
+		driveMotors[2].configSelectedFeedbackSensor(FeedbackDevice.Analog, 0, 0);
+		
+		// Set Motor Controller Peak Output Voltages & Set Motors to Coast
+		for(int i = 0; i < 4; i++){
+			driveMotors[i].configPeakOutputForward(1, 0);
+			driveMotors[i].configPeakOutputReverse(-1, 0);
+			driveMotors[i].setNeutralMode(NeutralMode.Coast);
+		}
+		
+		// Invert Left Motors
+		driveMotors[0].setInverted(true);
+		driveMotors[1].setInverted(true);
+	}
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	
+	public void setDriveOutput(double throttle, double angularPower){
+		double leftPWM = throttle + angularPower;
+		double rightPWM = throttle - angularPower;
+		
+		if(leftPWM > 1.0){
+			rightPWM -= (leftPWM - 1.0);
+			leftPWM = 1;
+		} else if (leftPWM < -1.0){
+			rightPWM -= (leftPWM + 1);
+			leftPWM = -1.0;
+		} else if(rightPWM > 1.0){
+			leftPWM -= (rightPWM - 1.0);
+			rightPWM = 1;
+		} else if (rightPWM < -1.0){
+			leftPWM -= (rightPWM + 1);
+			rightPWM = -1.0;
+		}
+		
+		driveMotors[0].set(ControlMode.PercentOutput, leftPWM);
+		driveMotors[2].set(ControlMode.PercentOutput, rightPWM);
+	}
+	
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
