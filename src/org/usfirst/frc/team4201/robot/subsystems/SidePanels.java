3c35c5bd4a6e4a172c1982a1d335ac9370431ccd
DESKTOP-BJDFJPV\Controls <vitruvianb@gmail.com>
2018-01-12 12:02:00 -0800

    Kai Lyou. Updated the Side Panel subsystem.

--- src/org/usfirst/frc/team4201/robot/subsystems/SidePanels.java
+++ src/org/usfirst/frc/team4201/robot/subsystems/SidePanels.java
@@ -2,24 +2,26 @@ package org.usfirst.frc.team4201.robot.subsystems;
 
 import org.usfirst.frc.team4201.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class SidePanels extends Subsystem{
 	
-	DoubleSolenoid sidePanels = new DoubleSolenoid(RobotMap.PCMOne, RobotMap.rightSidePanel, RobotMap.leftSidePanel);
+	Solenoid rightPanel = new Solenoid(RobotMap.PCMOne, RobotMap.rightSidePanel);
+	Solenoid leftPanel = new Solenoid(RobotMap.PCMOne, RobotMap.rightSidePanel);
 	
 	public SidePanels(){
 		
 	}
 	
 	public void deploySidePanels(){
-		sidePanels.set(Value.kForward);
+		rightPanel.set(true);
+		leftPanel.set(true);
 	}
 	
 	public void retractSidePanels(){
-		sidePanels.set(Value.kReverse);
+		rightPanel.set(false);
+		leftPanel.set(false);
 	}
 
 	@Override
